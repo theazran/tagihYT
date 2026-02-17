ef3f94e35fbb9437fae04f2f2faded688d0bb22b
tehazran <azranmu@gmail.com>
2026-02-17 08:16:54 -0300

    add admin route

--- server.js
+++ server.js
@@ -86,7 +86,11 @@ async function sendNotification(phone, message) {
 
 // --- Routes ---
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
 // 1. Create Transaction
